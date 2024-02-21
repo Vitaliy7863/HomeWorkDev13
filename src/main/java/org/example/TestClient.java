b9037c3f9bed4e4e7ffddb114856cec0548521c9
Віталій <vitaliy.shandar@gmail.com>
2024-02-21 20:53:19 +0200

    Commit

--- src/main/java/org/example/TestClient.java
+++ src/main/java/org/example/TestClient.java
@@ -3,9 +3,17 @@ package org.example;
 import org.example.dao.ClientCrudService;
 import org.example.model.Client;
 import org.example.util.HibernateUtil;
+import org.flywaydb.core.Flyway;
 
 public class TestClient {
     public static void main(String[] args) {
+        Flyway flyway = Flyway.configure().dataSource("jdbc:h2:./test;", "", "")
+                .locations("filesystem:src/main/resources/db.migration")
+                .baselineOnMigrate(true)
+                .load();
+
+        flyway.migrate();
+
         ClientCrudService crudClient = new ClientCrudService();
         System.out.println(crudClient.findAll());
 
@@ -13,7 +21,7 @@ public class TestClient {
         newClient.setName("Andriy");
         crudClient.save(newClient);
 
-        Client client = crudClient.findById(9);
+        Client client = crudClient.findById(8);
         System.out.println("Found client: " + client.getName());
 
         client.setName("Jane Doe");
